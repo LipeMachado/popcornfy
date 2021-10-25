a7d370a29505f8b29306aa9f2a426fafbf1f8576
LipeMachado <silvalipe765@gmail.com>
2021-10-24 22:50:33 -0300

    First Commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+import background from '../../images/background.png';
+
+export default function Header() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
