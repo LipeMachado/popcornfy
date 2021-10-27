89b423eb38a07ede1fff1999f3309a29fffde3cd
LipeMachado <silvalipe765@gmail.com>
2021-10-27 03:04:04 -0300

    Completed Design

--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
+/*import React, { useEffect, useState } from 'react';
 import './style.css';
 import Tmbd from '../Tmdb';
 
 export default function Homepage() {
     
 }
+*/
